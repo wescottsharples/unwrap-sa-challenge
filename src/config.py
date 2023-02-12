ea40ac64411cb327a1c00bf476a8fb005cc0d6cb
Wescott Sharples <wesc@ttsharpl.es>
2023-02-12 14:44:17 -0800

    access env vars directly

--- src/config.py
+++ src/config.py
@@ -7,11 +7,11 @@ import torch
 RANDOM_SEED = 42
 
 # MySQL database configuration
-HOST = os.getenv("MYSQL_HOST")
-PORT = os.getenv("MYSQL_PORT")
-USER = os.getenv("MYSQL_USER")
-PASSWORD = os.getenv("MYSQL_PASSWORD")
-DB = os.getenv("MYSQL_DB")
+HOST = os.environ["MYSQL_HOST"]
+PORT = os.environ["MYSQL_PORT"]
+USER = os.environ["MYSQL_USER"]
+PASSWORD = os.environ["MYSQL_PASSWORD"]
+DB = os.environ["MYSQL_DB"]
 CONNECTION_STRING = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"

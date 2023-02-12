489ef0994edb97b44d1184992b56b8640d1f9c57
Wescott Sharples <wesc@ttsharpl.es>
2023-02-12 14:06:49 -0800

    add funcs for loading train and test data

---
+++ src/data/make_dataset.py
@@ -0,0 +1,49 @@
+"""Dataset loading and preprocessing
+
+The dataset should be placed in the `data/raw` folder as a csv file
+and the path should be specified in the `config.py` file using the
+`DATASET_PATH` variable.
+"""
+
+
+import pandas as pd
+from datasets.arrow_dataset import Dataset
+from sklearn.model_selection import train_test_split
+
+from src.config import DATASET_PATH, RANDOM_SEED
+
+
+def load_df() -> pd.DataFrame:
+    """Load the dataset as a pandas DataFrame
+
+    Returns:
+        pd.DataFrame: Dataset as a pandas DataFrame
+    """
+    df = pd.read_csv(DATASET_PATH)
+    return df
+
+
+def split_dataset(df: pd.DataFrame) -> tuple:
+    """Split the dataset into train and test sets
+
+    Args:
+        df (pd.DataFrame): Dataset as a pandas DataFrame
+
+    Returns:
+        tuple: Train and test datasets as pandas DataFrames
+    """
+    train, test = train_test_split(df, test_size=0.2, random_state=RANDOM_SEED)
+    return train, test
+
+
+def load_dataset() -> dict:
+    """Load the dataset as a HuggingFace dataset
+
+    Returns:
+        dict: HuggingFace dataset
+    """
+    df = load_df()
+    train, test = split_dataset(df)
+    train_ds = Dataset.from_pandas(train)
+    test_ds = Dataset.from_pandas(test)
+    return {"train": train_ds, "test": test_ds}

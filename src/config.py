08d39c7dc625cc4919deda6f99d298bde9aa1420
Wescott Sharples <wesc@ttsharpl.es>
2023-02-13 16:04:47 -0800

    suppress hf warnings and reformat

--- src/config.py
+++ src/config.py
@@ -1,26 +1,41 @@
+"""Configuration file for the application."""
+
 import os
 from pathlib import Path
 
 import torch
+from dotenv import load_dotenv
+from transformers import logging
 
-# Defines the configuration for the application
+load_dotenv()  # Load environment variables from .env file
+logging.set_verbosity_error()  # Disable transformers warnings
+
+# Training Parameters
 RANDOM_SEED = 42
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+VALID_SENTIMENTS = ["NEGATIVE", "NEUTRAL", "POSITIVE"]
+LABEL_MAPPING = {0: "NEGATIVE", 1: "NEUTRAL", 2: "POSITIVE"}
+OPENAI_MODEL_NAME = "text-davinci-003"  # Most expensive OpenAI model
 
-# MySQL database configuration
-HOST = os.environ["MYSQL_HOST"]
-PORT = os.environ["MYSQL_PORT"]
-USER = os.environ["MYSQL_USER"]
-PASSWORD = os.environ["MYSQL_PASSWORD"]
-DB = os.environ["MYSQL_DB"]
-CONNECTION_STRING = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+# MySQL Configuration
+HOST = os.environ.get("MYSQL_HOST", None)
+PORT = os.environ.get("MYSQL_PORT", None)
+USER = os.environ.get("MYSQL_USER", None)
+PASSWORD = os.environ.get("MYSQL_PASSWORD", None)
+DB = os.environ.get("MYSQL_DB", None)
+if all([HOST, PORT, USER, PASSWORD, DB]):
+    CONNECTION_STRING = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+else:
+    CONNECTION_STRING = None
+    print("No MySQL database configuration found. Add environment variables to .env file.")
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
+# Paths
 PARENT_DIR = Path(__file__).parent.parent
-SENTIMENT_ANNOTATIONS_CSV = PARENT_DIR / "data" / "raw" / "sentiment_annotations.csv"
+DATA_DIR = PARENT_DIR / "data"
+CUSTOM_MODELS_DIR = DATA_DIR / "models"
+SENTIMENT_ANNOTATIONS_CSV = DATA_DIR / "raw" / "sentiment_annotations.csv"
 DATASET_PATH = SENTIMENT_ANNOTATIONS_CSV
-LABEL_MAPPING = {
-    0: "NEGATIVE",
-    1: "NEUTRAL",
-    2: "POSITIVE",
-}  # all models from the HuggingFace library use this mapping, so we will too
+SETFIT_MODEL_PATH = CUSTOM_MODELS_DIR / "setfit_model.pkl"
+
+# Create directories if they don't exist
+CUSTOM_MODELS_DIR.mkdir(parents=True, exist_ok=True)
